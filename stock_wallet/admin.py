254371e43c8bf3f7af5e66ca35c630bbcde4610a
Kazaname <suriiw@gmail.com>
2020-03-22 20:49:45 +0100

    Register models in admin site. Created tabulat in line view in admin site.

--- stock_wallet/admin.py
+++ stock_wallet/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Wallet, PurchasedShare, Dividend
 
-# Register your models here.
+
+class DividendTabularInLine(admin.TabularInline):
+    model = Dividend
+
+class PurchasedShareAdmin(admin.ModelAdmin):
+    inlines = [DividendTabularInLine]
+    list_display = [ 'company', 'short_name', 'ammount', 'price_for_one_stock', 'price_for_stocks', 'created_at', 'updated_at']
+
+class StockRecordTabularInLine(admin.TabularInline):
+    model = PurchasedShare
+
+class WalletAdmin(admin.ModelAdmin):
+    inlines = [StockRecordTabularInLine]
+    list_display = ['name', 'user', 'cash', 'total_profit', 'is_private', 'slug']
+
+class SummaryPurchasedShareAdmin(admin.ModelAdmin):
+    list_display = ['company', 'short_name', 'ammount', 'price_for_one_stock', 'price_for_stocks']
+
+
+admin.site.register(Wallet, WalletAdmin)
+admin.site.register(PurchasedShare, PurchasedShareAdmin)
