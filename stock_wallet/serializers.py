68df4fd80b0d73545c60e65eceb483f8a95555e1
Kazaname <suriiw@gmail.com>
2020-03-22 21:01:17 +0100

    Created serializer file. Based on Udemy rest framework course created serializers class. (not implemented - I need work with it)

---
+++ stock_wallet/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Wallet, PurchasedShare
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = ['id', 'user', 'name', 'total_profit', 'created_at', 'updated_at']
+
+class PurchasedShareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchasedShare
+        fields = ['id', 'user', 'wallet', 'company', 'short_name', 'ammount',
+                  'price_for_one_stock', 'price_for_stocks', 'created_at', 'updated_at']
