27b3a386af57b6ddf76dc72c8b2e5fb900e77191
Kazaname <suriiw@gmail.com>
2020-03-22 20:54:00 +0100

    Created form file. Udemy eCommerce course ContactForm class.

---
+++ stock_wallet/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control",
+                                                             "placeholder": "Your full name"}))
+    email = forms.EmailField()
+    content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control",
+                                                           "placeholder": "Your message"}))
