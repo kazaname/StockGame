64c52b62cd6e3df11f2715560dd23433fca27988
Marek Polak <marek.polak@globallogic.com>
2020-03-29 11:25:20 +0200

    Created new form class WalletModelForm.

--- stock_wallet/forms.py
+++ stock_wallet/forms.py
@@ -1,4 +1,18 @@
 from django import forms
+from .models import Wallet
+
+class WalletModelForm(forms.ModelForm):
+    class Meta:
+        model = Wallet
+        fields = [
+            'name',
+            'cash',
+            'minimum_commission',
+            'percentage_commission',
+            'is_private',
+            'comment',
+        ]
+
 
 class ContactForm(forms.Form):
     fullname = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control",
