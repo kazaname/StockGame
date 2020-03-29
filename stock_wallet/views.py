1fd65d8b4b68051f37671a1969d26dde161962f5
Marek Polak <marek.polak@globallogic.com>
2020-03-29 11:20:56 +0200

    Created new view class WalletCreateView. Changed ListView class from PublicWalletsListView on WalletsListView. Changed ListView class from PublicWalletsDetailView on WalletsDetailView.

--- stock_wallet/views.py
+++ stock_wallet/views.py
@@ -1,18 +1,19 @@
 from django.core.exceptions import PermissionDenied
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
-from .forms import ContactForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, View, CreateView
+from .forms import ContactForm, WalletModelForm
 from .models import Wallet, PurchasedShare
 # from rest_framework import viewsets
 # from .serializers import WalletSerializer, PurchasedShareSerializer
 
-class PublicWalletsListView(ListView):
+class WalletsListView(ListView):
     queryset = Wallet.objects.filter(is_private=False)
     template_name = "stock_wallet/wallets_list.html"
 
     def get_context_data(self, *args, **kwargs):
-        context = super(PublicWalletsListView, self).get_context_data(*args, **kwargs)
+        context = super(WalletsListView, self).get_context_data(*args, **kwargs)
         if self.request.user.is_authenticated:
             private_wallets = Wallet.objects.filter(user=self.request.user ,is_private=True)
         else:
@@ -29,9 +30,8 @@ class PublicWalletsListView(ListView):
     # def get_queryset(self):
     #     return Wallet.objects.filter(is_private=False)
 
-class PublicWalletsDetailView(DetailView):
+class WalletsDetailView(DetailView):
     model = Wallet
-    # queryset = Wallet.objects.all()
     template_name = "stock_wallet/wallet_detail.html"
 
     # Poniższa metoda pozwala na wyciągnięcie z kwargs wartości id i pobranie objektu o podanym numerze id
@@ -54,6 +54,19 @@ class PublicWalletsDetailView(DetailView):
     #     context = super(PublicWalletsDetailView, self).get_context_data(*args, **kwargs)
     #     # context['abc'] = 123
     #     return context
+
+class WalletCreateView(CreateView):
+    template_name = 'stock_wallet/create_wallet.html'
+    form_class = WalletModelForm
+    success_url = reverse_lazy('stock_wallet:public_wallets')
+
+    def post(self, request, *args, **kwargs):
+        form = WalletModelForm(request.POST)
+        if form.is_valid():
+            form.instance.user = self.request.user
+            form.save()
+        return super(WalletCreateView, self).form_valid(form)
+
 
 # class WalletViewSet(viewsets.ModelViewSet):
 #     queryset = Wallet.objects.all()
