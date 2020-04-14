c9c01ed3c1e67b275849bde3ba53cd5447381ac6
Kazaname <suriiw@gmail.com>
2020-04-14 07:24:37 +0200

    An url has been added to the records of a specific company purchased.

--- stock_wallet/urls.py
+++ stock_wallet/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include, re_path
-from .views import WalletsListView, WalletsDetailView, WalletCreateView
+from .views import WalletsListView, WalletsDetailView, WalletCreateView, PurchasedShareListView
 # from .views import WalletViewSet, PurchasedShareViewSet
 from rest_framework import routers
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', WalletsListView.as_view(), name='public_wallets'),
     path('create_wallet', WalletCreateView.as_view(), name='create_wallet'),
     path('<slug:slug>/', WalletsDetailView.as_view(), name='wallet_detail'),
+    path('<slug:slug>/<str:company_name>/', PurchasedShareListView.as_view(), name='share_list'),
 
     # path('<int:id>/', PublicWalletsDetailView.as_view(), name='wallet_detail'), URL powiązany jest z detail view get_object
 ]
