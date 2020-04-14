5897b3f9f0e4697ddca6914efa2acde0898f6c95
Kazaname <suriiw@gmail.com>
2020-04-14 07:19:53 +0200

    Models - PurchasedShare added doc string. Added status row. Changed rows names in case to be more clear. Signal - Changed function names in case to be more clear Admin file - names were matched to changes made in models.py file

--- stock_wallet/admin.py
+++ stock_wallet/admin.py
@@ -7,7 +7,7 @@ class DividendTabularInLine(admin.TabularInline):
 
 class PurchasedShareAdmin(admin.ModelAdmin):
     inlines = [DividendTabularInLine]
-    list_display = [ 'company', 'short_name', 'ammount', 'price_for_one_stock', 'price_for_stocks', 'created_at', 'updated_at']
+    list_display = [ 'company_name', 'short_name', 'amount', 'price_per_one', 'amount_invested', 'created_at', 'updated_at']
 
 class StockRecordTabularInLine(admin.TabularInline):
     model = PurchasedShare
@@ -17,7 +17,7 @@ class WalletAdmin(admin.ModelAdmin):
     list_display = ['name', 'user', 'cash', 'total_profit', 'is_private', 'slug', 'created_at', 'updated_at']
 
 class SummaryPurchasedShareAdmin(admin.ModelAdmin):
-    list_display = ['company', 'short_name', 'ammount', 'price_for_one_stock', 'price_for_stocks']
+    list_display = ['company_name', 'short_name', 'amount', 'price_per_one', 'amount_invested']
 
 
 admin.site.register(Wallet, WalletAdmin)
--- stock_wallet/models.py
+++ stock_wallet/models.py
@@ -11,7 +11,7 @@ class Wallet(models.Model):
     total_profit            = models.DecimalField(max_digits=28, decimal_places=9, default=0)
     cash                    = models.DecimalField(max_digits=28, decimal_places=4, default=0)
     minimum_commission      = models.DecimalField(max_digits=8, decimal_places=4, blank=True, null=True,
-                                                  validators=[MinValueValidator(0),])
+                                                  validators=[MinValueValidator(0), ])
     percentage_commission   = models.FloatField(blank=True, null=True)
     is_private              = models.BooleanField(default=True)
     slug                    = models.SlugField(blank=True, null=True)
@@ -22,27 +22,33 @@ class Wallet(models.Model):
     def __str__(self):
         return self.name
 
-
     class Meta:
         ordering = ['-updated_at', 'total_profit']
         unique_together = [['user', 'name']]
 
 
 class PurchasedShare(models.Model):
+    """
+    Model DB odpowiadający zakupionym akcjom na GPW.
+    Model połączony jest z portfelem ( model Wallet) oraz urzytkownikiem (model User)
+    Wiersz amount_invested aktualizowany jest automatycznie poprzez sygnał amount_invested_calculator
+    """
     wallet                  = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     user                    = models.ForeignKey(User, on_delete=models.CASCADE)
-    company                 = models.CharField(max_length=120)
+    company_name            = models.CharField(max_length=120)
     short_name              = models.CharField(max_length=10)
-    ammount                 = models.IntegerField(validators= [MinValueValidator(1),])
-    price_for_one_stock     = models.DecimalField(max_digits=15, decimal_places=9)
-    price_for_stocks        = models.DecimalField(max_digits=19, decimal_places=9, blank=True, null=True)
-    share_price             = models.DecimalField(max_digits=12, decimal_places=4, blank=True, null=True)
-    comment                 = models.TextField(max_length=500, blank=True, null=True)
+    amount                  = models.IntegerField(validators= [MinValueValidator(1),])
+    price_per_one           = models.DecimalField(max_digits=15, decimal_places=9)
+    amount_invested         = models.DecimalField(max_digits=19, decimal_places=9, blank=True, null=True)
+    current_price           = models.DecimalField(max_digits=12, decimal_places=4, blank=True, null=True)
+    # comment                 = models.TextField(max_length=500, blank=True, null=True)
+    status                  = models.BooleanField(default=True)
     created_at              = models.DateTimeField(auto_now_add=True)
     updated_at              = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.company
+        return self.company_name
+
 
 class Dividend(models.Model):
     stockrecord             = models.ForeignKey(PurchasedShare, on_delete=models.CASCADE, blank=True, null=True)
@@ -55,17 +61,19 @@ class Dividend(models.Model):
     created_at              = models.DateTimeField(auto_now_add=True)
     updated_at              = models.DateTimeField(auto_now=True)
 
+
 def create_slug_name(sender, instance, *args, **kwargs):
     instance.name = instance.name.title()
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
 
+
 def total_profit_calculator(sender, instance, *args, **kwargs):
     return
 
-def stock_profit_calculator(sender, instance, *args, **kwargs):
-    instance.price_for_stocks = instance.ammount * instance.price_for_one_stock
-    print(instance.price_for_stocks)
+def amount_invested_calculator(sender, instance, *args, **kwargs):
+
+    instance.amount_invested = instance.amount * instance.price_per_one
 
 pre_save.connect(create_slug_name, sender=Wallet)
-pre_save.connect(stock_profit_calculator, sender=PurchasedShare)
+pre_save.connect(amount_invested_calculator, sender=PurchasedShare)
